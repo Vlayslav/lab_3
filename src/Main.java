31664d79cfa5ac59dc020275c30928ab2f43ca7a
Administrator <vlad132hds@gmail.com>
2026-04-03 14:36:14 +0300

    super-puper: feature 1.2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
 void main() {
     System.out.print("Enter val: ");
     Scanner sc = new Scanner(System.in);
-    String strVal = sc.next();
+    String strVal = sc.nextLine();
     char[] arrVal = strVal.toCharArray();
 
     String invertVal = "";
@@ -15,4 +15,45 @@ void main() {
         invertVal+=arrVal[i];
     }
     System.out.println("звороотній ряд: "+invertVal);
+
+    /*
+    Додаток
+    З клавіатури вводиться текстовий рядок. Скласти програму, яка підраховує кількість розділових знаків у тексті;
+    виводить всі слова, що мають парну кількість літер; міняє місцями першу і останню літери кожного слова.
+     */
+
+    System.out.print("\nEnter val: ");
+    Scanner sc2 = new Scanner(System.in);
+    String text = sc2.nextLine();
+
+    String cleanText = text.replaceAll("\\W", " ").replaceAll("\\d", "")
+            .replaceAll("\\s+", " ");
+    String dirtyText = text.replaceAll("\\w", "").replaceAll("\\s+", "");
+
+    //виведення
+    System.out.println("розділових знаків: "+dirtyText.length());
+
+    System.out.println("парна к-сть літер: ");
+    String tempWord="";
+    for (char element:cleanText.toCharArray()){
+        if (element!=' ') tempWord+=element;
+        else if(tempWord.toCharArray().length%2==0){
+            System.out.println("\t"+tempWord+" - "+tempWord.toCharArray().length);
+            tempWord="";
+        }else tempWord="";}
+
+    System.out.println("переміщені літери слів: ");
+    String fullWord="";
+    for (char element:cleanText.toCharArray()){
+        if (element!=' ') fullWord += element;
+        if (element==' ' && fullWord!=""){
+            char[] wordArr = fullWord.toCharArray();
+            char firstLetter = wordArr[0];
+            wordArr[0]=wordArr[wordArr.length-1];
+            wordArr[wordArr.length-1]=firstLetter;
+            System.out.println("\t"+ String.valueOf(wordArr));
+            fullWord="";}}
+
+    System.out.println("cleanText: |"+cleanText+"|");
+    System.out.println("dirtyText: |"+dirtyText+"|");
 }
